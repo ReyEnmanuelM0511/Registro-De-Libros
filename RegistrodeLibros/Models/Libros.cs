55589cd9713b5cb1bcfeece95e45af4e206d686c
Rey Enmanuel Marte Picherdo <email>
2026-09-14 17:28:58 -0400

    Requerimientos de los campos de la base de Datos

--- RegistrodeLibros/Models/Libros.cs
+++ RegistrodeLibros/Models/Libros.cs
@@ -4,7 +4,10 @@ public class Libros
 {
     [Key]
     public int LibroID { get; set;}
+    [Required(ErrorMessage = "El campo de Titulo es Obligatorio")]
     public string? Titulo {get; set;}
+    [Required(ErrorMessage = "El campo de Autor es Requerido")]
     public string? Autor {get; set;}
+    [Required(ErrorMessage ="El campo de AnoPublicacion es obligatorio")]
     public string? AnoPublicacion {get; set;}
 }
