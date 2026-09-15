475d0297c6569f222c5ab722f4d857e35e418b3a
Rey Enmanuel Marte Picherdo <email>
2026-09-14 21:09:13 -0400

    cosas hechas en clase

---
+++ RegistrodeLibros/Models/Jugadores.cs
@@ -0,0 +1,13 @@
+﻿namespace RegistrodeLibros.Models;
+using System.ComponentModel.DataAnnotations;
+
+public class Jugadores
+{
+    [Key]
+    public int Id { get; set; }
+    [Required(ErrorMessage = "El campo de Nombre es obligatorio")]
+    public string? Nombre { get; set; }
+    public int Ranking { get; set; }
+
+}
+
